42a3794c3df9d560209d96c16dc400956abf434c
Rafael Paiva <raffaelgustavo@hotmail.com>
2019-08-20 14:04:57 -0300

    adicionado comentarios no código

--- index.php
+++ index.php
@@ -6,25 +6,25 @@ $usuarios = Usuario::getList(); // puxa a lista de usuários
 
 $headers = array();
 
-foreach ($usuarios[0] as $key => $value) {
+foreach ($usuarios[0] as $key => $value) { // percorre o array listando a linha e atribuindo as chaves da linha
 	array_push($headers, ucfirst($key));
 }
 
-$file = fopen("usuarios.csv", "w+");
+$file = fopen("usuarios.csv", "w+"); // cria o arquivo e inicia a escrita
 
-fwrite($file, implode(";",$headers). "\r\n");
+fwrite($file, implode(";",$headers). "\r\n"); // escreve os valores do array separado por ;
 
-foreach ($usuarios as $row) {
+foreach ($usuarios as $row) { //percorre a lista de usuarios em cada linha
 	
 	$data = array();
 
-	foreach ($row as $key => $value) {
+	foreach ($row as $key => $value) { //em cada linha salva o valor das colunas
 	
 		array_push($data, $value);	
 
 	}//acaba foreach de coluna
 
-	fwrite($file, implode(";", $data) ."\r\n");
+	fwrite($file, implode(";", $data) ."\r\n"); // escreve os valores do array separado por ;
 
 }// acaba foreach de linha
 
